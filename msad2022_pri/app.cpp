7dbe639378b7830cfe9a170f87afb86178a20e13
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-11 22:09:11 +0900

    黒はいけるのか？

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -624,7 +624,7 @@ void main_task(intptr_t unused) {
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<IsTimeEarned>(10000000) // break after 10 seconds
                 .leaf<RunAsInstructed>(50,50,0.0)    
-                .leaf<IsColorDetected>(CL_GREEN)    
+                .leaf<IsColorDetected>(CL_BLACK) //    
             .end()        
             .leaf<StopNow>()
             .leaf<IsTimeEarned>(3000000) // wait 3 seconds
