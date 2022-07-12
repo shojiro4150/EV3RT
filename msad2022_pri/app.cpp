170abfada66e5a763c47fc23f086f94302d96000
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-12 21:02:13 +0900

    f

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -275,7 +275,7 @@ public:
                 }
                 break;
             case CL_BLUE:
-                if (cur_rgb.r <= 30 && cur_rgb.g <= 40 && cur_rgb.b <= 50) {
+                if (cur_rgb.r <= 30 && cur_rgb.g >= 30 && cur_rgb.b <= 50) {
                     _log("ODO=%05d, CL_BLUE detected.", plotter->getDistance());
                     _log("r=%d g=%d b=%d",cur_rgb.r,cur_rgb.g,cur_rgb.b);
                     return Status::Success;
