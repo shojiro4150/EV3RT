c892c32c876670c7c30ff69e884ca79e59f6831a
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-18 11:19:31 +0900

    d

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -626,12 +626,8 @@ void main_task(intptr_t unused) {
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<IsTimeEarned>(2000000) // break after 10 seconds
-                .leaf<RunAsInstructed>(-60,-70,0.0)    
+                .leaf<RunAsInstructed>(-60,-80,0.0)    
                 .leaf<IsColorDetected>(CL_BLACK)  
-            .end()
-            .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsTimeEarned>(500000) // break after 10 seconds
-                .leaf<RunAsInstructed>(60,60,0.0)    
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<IsTimeEarned>(1000000) // break after 10 seconds
