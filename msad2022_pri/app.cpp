7e542f91138db3dabe87f6ceab7f6e95af9e2942
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-18 15:53:40 +0900

    h

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -652,7 +652,7 @@ void main_task(intptr_t unused) {
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<IsTimeEarned>(5000000)
-                .leaf<TraceLine>(40, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)  
+                .leaf<TraceLine>(48, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)  
                 .leaf<IsColorDetected>(CL_WHITE)  
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
