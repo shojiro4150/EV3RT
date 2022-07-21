10b7d356da2df00fbf6f06d0033346c3d483e49b
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-21 20:31:13 +0900

    h

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -642,7 +642,7 @@ void main_task(intptr_t unused) {
                 .leaf<RunAsInstructed>(50,50,0.0)      
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsTimeEarned>(1500000) // break after 10 seconds
+                .leaf<IsTimeEarned>(1000000) // break after 10 seconds
                 .leaf<RunAsInstructed>(70,40,0.0)      
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
