51a55aebe0a8c57056594a41eb012c4c80faea98
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-28 00:27:28 +0900

    d

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -894,12 +894,15 @@ void main_task(intptr_t unused) {
     tr_block_d = (BrainTree::BehaviorTree*) BrainTree::Builder()
         .composite<BrainTree::MemSequence>()
             .composite<BrainTree::ParallelSequence>(1,3)
+                .leaf<SetArmPosition>(10, 40) 
+                .leaf<IsTimeEarned>(1000000) 
+            .end()
+            .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<TraceLine>(40, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)  
                 .leaf<IsTimeEarned>(1000000) 
             .end()
             .leaf<StopNow>()
             .leaf<IsTimeEarned>(30000000) // wait 3 seconds
-            .leaf<SetArmPosition>(10, 40)
         .end()
         .build();
 
