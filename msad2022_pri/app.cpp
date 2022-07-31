211994540dc31dd9435ec1612cb390f3f8c45ce7
ttboy <tanaka.no.tatsuya@gmail.com>
2022-07-31 11:49:35 +0900

    g

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -1180,7 +1180,7 @@ void main_task(intptr_t unused) {
                 .leaf<RunAsInstructed>(40,40,0.0)  //グレー検知後、丸穴あき部分があるため少し前進    
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsTimeEarned>(630000) // break after 10 seconds
+                .leaf<IsTimeEarned>(650000) // break after 10 seconds
                 .leaf<RunAsInstructed>(-50,50,0.0) //左に旋回。ライントレース準備。
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
