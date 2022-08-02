d1bd81021b7dbcdf05c4db1a72625fae4bcdc764
ttboy <tanaka.no.tatsuya@gmail.com>
2022-08-02 20:47:17 +0900

    f

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -1253,7 +1253,7 @@ void main_task(intptr_t unused) {
                 .leaf<RunAsInstructed>(40,40,0.0)  //グレー検知後、丸穴あき部分があるため少し前進    
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsAngleSmaller>(-30)
+                .leaf<IsAngleSmaller>(-20)
                 .leaf<RunAsInstructed>(-50,50,0.0) //左に旋回。ライントレース準備。
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
