cece2a90de3ae57834349dbb7fd56fcebaa94f33
ttboy <tanaka.no.tatsuya@gmail.com>
2022-08-05 20:53:42 +0900

    f

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -1236,7 +1236,7 @@ void main_task(intptr_t unused) {
                 .leaf<IsColorDetected>(CL_BLACK)  
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsAngleSmaller>(-30)
+                .leaf<IsAngleSmaller>(-20)
                 .leaf<RunAsInstructed>(-30,60,0.0)      
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
