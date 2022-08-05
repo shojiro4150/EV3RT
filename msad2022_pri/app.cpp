4ca5d96c24df881ad72699995b7cc4e6ee6dcb5e
ttboy <tanaka.no.tatsuya@gmail.com>
2022-08-05 21:22:57 +0900

    f

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -1105,7 +1105,7 @@ void main_task(intptr_t unused) {
                 .leaf<IsColorDetected>(CL_BLUE2)     
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsTimeEarned>(700000) // 検知後、斜め右前まで回転(ブロックを離さないように)
+                .leaf<IsTimeEarned>(900000) // 検知後、斜め右前まで回転(ブロックを離さないように)
                 .leaf<RunAsInstructed>(-55,60,0.0)      
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
