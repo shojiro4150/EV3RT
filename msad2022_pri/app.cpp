09ccd9756f8b2eef063870f68ce60a7ce315cc65
ttboy <tanaka.no.tatsuya@gmail.com>
2022-08-08 20:22:32 +0900

    f

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -1129,7 +1129,7 @@ void main_task(intptr_t unused) {
                 .leaf<RunAsInstructed>(45,45,0.0)      
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)   
-                .leaf<IsTimeEarned>(710000) // 黄色検知後、方向立て直す。
+                .leaf<IsTimeEarned>(705000) // 黄色検知後、方向立て直す。
                 .leaf<RunAsInstructed>(30,80,0.0) 
                 .leaf<IsColorDetected>(CL_RED)   
             .end()
