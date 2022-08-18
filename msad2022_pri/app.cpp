5dc106a5dd54749c41ff37c72445bacffd36ce62
ymnkmstk <ymnkbzaioi@gmail.com>
2022-08-18 21:27:43 +0900

    njnj

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -313,7 +313,7 @@ public:
                 }
                 break;
             case CL_JETBLACK_YMNK:
-                if (cur_rgb.r <=6 && cur_rgb.g <=6 && cur_rgb.b <=6) { 
+                if (cur_rgb.r <=6 && cur_rgb.g <=6 && cur_rgb.b <=7) { 
                     _log("ODO=%05d, CL_JETBLACK_YMNK detected.", plotter->getDistance());
                     return Status::Success;
                 }
