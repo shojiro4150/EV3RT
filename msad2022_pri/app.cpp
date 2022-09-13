1a9ca33a03714e85dd49f7b61b96f9c7d2d59b01
ttboy <tanaka.no.tatsuya@gmail.com>
2022-09-13 20:49:44 +0900

    f

--- msad2022_pri/app.cpp
+++ msad2022_pri/app.cpp
@@ -1357,7 +1357,7 @@ void main_task(intptr_t unused) {
                 .leaf<IsColorDetected>(CL_BLUE) 
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
-                .leaf<IsTimeEarned>(900000) // 後ろ向き走行。狙いは黒線。
+                .leaf<IsTimeEarned>(975000) // 後ろ向き走行。狙いは黒線。
                 .leaf<RunAsInstructed>(-30,
                                        -80,
                                        0.0)      
@@ -1378,13 +1378,13 @@ void main_task(intptr_t unused) {
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<IsTimeEarned>(900000) // 黒線検知後、ライントレース準備
                 .leaf<RunAsInstructed>(-30,
-                                       60,
+                                       55,
                                        0.0)     
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<TraceLine>(35,
                                  GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL)  
-                .leaf<IsTimeEarned>(1000000) // 黒線検知後、ライントレース準備
+                .leaf<IsTimeEarned>(1500000) // 黒線検知後、ライントレース準備
             .end()
             .composite<BrainTree::ParallelSequence>(1,3)
                 .leaf<TraceLine>(40, 
